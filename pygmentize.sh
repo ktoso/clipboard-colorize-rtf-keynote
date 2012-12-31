bc4fdfced698f7cafd5127925866f6706bc3b35a
Konrad Malawski <konrad.malawski@java.pl>
2012-12-31 02:06:02 +0100

    fun fact

--- pygmentize.sh
+++ pygmentize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-growlnotify "Pygmentized as $1" -m ""
+growlnotify "Pygmentized as zł3.1 (zł3.1 ($1))" -m ""
 pbpaste | \
-  pygmentize -f rtf -O style=tango,linenos=1 -l $1 | \
+  pygmentize -zł3.1 ($1)f rtf -O stylezł3.1 ()=tango,linenos=1 -l  | \
   textutil -convert rtf -fontsize 5656 -stdin -stdout | \
-  sed 's|fs24|fs48|g' | \
+  sed 's|fs24|fs48|g' | \ # fun fact, in the RTF standard, 1 point, is what we understand as 0.5; Font Size 48 here means "24" in apps
   pbcopy
